52bf1af40e30f950c693c16b098dc95e2752f642
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-19 05:37:28 +0600

    ConfirmDeleteModal connect to server

--- src/components/ConfirmDeleteModal.jsx
+++ src/components/ConfirmDeleteModal.jsx
@@ -1,22 +1,39 @@
+import axios from "axios";
 import React from "react";
 
-const ConfirmDeleteModal = ({ deleteItem }) => {
-  console.log(deleteItem);
+const ConfirmDeleteModal = ({ setDeleteItem, refetch, deleteItem }) => {
+  const handleDeleteTodo = async () => {
+    try {
+      const { data } = await axios.delete(
+        `http://localhost:5000/todo/${deleteItem._id}`
+      );
+
+      if (data.deletedCount) refetch();
+      setDeleteItem({});
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-
-      <input type="checkbox" id="delete-modal" class="modal-toggle" />
-      <div class="modal">
-        <div class=" ">
-          <div class="card w-96 bg-base-100 shadow-xl">
-            <div class="card-body">
-              <h2 class="card-title text-warning">Are you sure?</h2>
+      <input type="checkbox" id="delete-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className=" ">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title text-warning">Are you sure?</h2>
               <p className=" text-gray-50">You can't undo after delete.</p>
-              <div class="modal-action">
-                <label class="btn btn-sm bg-red-700 hover:bg-red-700 hover:border-red-700 text-gray-50">
+              <div className="modal-action">
+                <label
+                  onClick={handleDeleteTodo}
+                  className="btn btn-sm bg-red-700 hover:bg-red-700 hover:border-red-700 text-gray-50"
+                >
                   Delete
                 </label>
-                <label for="delete-modal" class="btn btn-sm  btn-secondary">
+                <label
+                  htmlFor="delete-modal"
+                  className="btn btn-sm  btn-secondary"
+                >
                   Cancel
                 </label>
               </div>
