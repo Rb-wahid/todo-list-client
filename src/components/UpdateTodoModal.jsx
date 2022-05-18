15547365cab0d4b69acc63d33f04e294a284f801
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-19 05:36:15 +0600

    UpdateTodoModal connect to server

--- src/components/UpdateTodoModal.jsx
+++ src/components/UpdateTodoModal.jsx
@@ -1,6 +1,11 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const UpdateTodoModal = ({ updateItem: { title, description } }) => {
+const UpdateTodoModal = ({
+  refetch,
+  setUpdateItem,
+  updateItem: { _id, title, description },
+}) => {
   const [updateTitle, setUpdateTitle] = useState("");
   const [updateDescription, setUpdateDescription] = useState("");
 
@@ -10,37 +15,55 @@ const UpdateTodoModal = ({ updateItem: { title, description } }) => {
     setUpdateDescription(description);
   }, [title, description]);
 
-  console.log(updateTitle, updateDescription);
+  const handleUpdateTodo = async () => {
+    try {
+      const todoData = {
+        id: _id,
+        title: updateTitle,
+        description: updateDescription,
+        isComplete: false,
+      };
+      const { data } = await axios.put(`http://localhost:5000/todo/update`, {
+        todoData,
+      });
+      if (data.modifiedCount) {
+        refetch();
+        setUpdateItem({});
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-      <input type="checkbox" id="update-modal" class="modal-toggle" />
-      <div class="modal">
-        <div class=" ">
-          <div class="card w-96 bg-accent shadow-xl">
-            <div class="card-body">
+      <input type="checkbox" id="update-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className=" ">
+          <div className="card w-96 bg-accent shadow-xl">
+            <div className="card-body">
               <input
                 type="text"
                 defaultValue={updateTitle}
                 onChange={(e) => setUpdateTitle(e.target.value)}
                 required
-                class="input input-bordered w-full max-w-xs"
+                className="input input-bordered w-full max-w-xs"
               />
               <textarea
-                class="textarea textarea-bordered h-24"
+                className="textarea textarea-bordered h-24"
                 defaultValue={updateDescription}
                 onChange={(e) => setUpdateDescription(e.target.value)}
                 placeholder="Description"
               ></textarea>
-              <div class="modal-action justify-center">
+              <div className="modal-action justify-center">
                 <label
-                  //   onClick={() => setOpenAddModal(false)}
+                  onClick={handleUpdateTodo}
                   type="submit"
-                  class="btn btn-sm  btn-secondary"
+                  className="btn btn-sm  btn-secondary"
                 >
                   update
                 </label>
-                <label htmlFor="update-modal" class="btn btn-sm  btn-ghost">
+                <label htmlFor="update-modal" className="btn btn-sm  btn-ghost">
                   Cancel
                 </label>
               </div>
